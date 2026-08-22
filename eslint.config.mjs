69819343cc4cd8456098278017e506173534886e
Adiazgomez68 <adiazgomez68@correo.unicordoba.edu.co>
2026-08-22 10:44:48 -0500

    add: initial config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
-import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import eslintPluginJsxA11y from "eslint-plugin-jsx-a11y";
+
+import { defineConfig, globalIgnores } from "eslint/config";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -13,6 +15,8 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  // Accessibility plugin recommended rules to ensure that our web app is accessible to all users
+  eslintPluginJsxA11y(),
 ]);
 
 export default eslintConfig;
